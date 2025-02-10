877c1e8cd823a45037a1b4b8faa062f115aa9a22
DharmarajuG <dharmarajurps@gmail.com>
2025-02-10 19:58:45 +0530

    This is for vpc

---
+++ Test/variables.tf
@@ -0,0 +1,22 @@
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+variable "common_tags" {
+  default = {
+    Project = "roboshop"
+    Environment = "dev"
+    Terraform = "true"
+  }
+}
+
+variable "vpc_tags" {
+  default = {}
+}
+
+variable "project_name" {
+  default = "roboshop"
+}
+
+variable "environment" {
+  default = "dev"
+}
