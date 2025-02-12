552ead2d5535aa2d4c7cbc894270328f0c33e18e
DharmarajuG <dharmarajurps@gmail.com>
2025-02-12 08:30:34 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -6,15 +6,14 @@ output "vpc_id" {
   value = aws_vpc.main.id
 }
 
-output "public_subnets" {
+output "public_subnet_ids" {
   value = aws_subnet.public[*].id
-}   
+}
 
-output "private_subnets" {
+output "private_subnet_ids" {
   value = aws_subnet.private[*].id
 }
 
-output "database_subnets" {
+output "database_subnet_ids" {
   value = aws_subnet.database[*].id
 }
-
